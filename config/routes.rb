0d73695387227c2c6dd57d010ae3ac3a2ddc24c3
Jonas Preisler <jonas.preisler@gmail.com>
2018-05-19 04:39:24 +0800

    bootstrapped devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+    root 'home#index'
+	get 'home/about'
+	get 'home/lookup'
+	post "/home/index" => 'home/index'
 end
